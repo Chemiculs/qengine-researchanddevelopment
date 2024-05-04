202308dddf0d43f0b91eb2ec1cf97f246aab857b
lane graham <lanegraham3@gmail.com>
2024-05-04 13:08:02 -0700

    appended documentation

--- collisionengine/entrypoint.cxx
+++ collisionengine/entrypoint.cxx
@@ -51,7 +51,7 @@ using namespace qengine;
 static void* data_ptr;
 static size_t data_size;
 static std::thread data_thread;
-    
+
 bool thread_state_flag = true;
 #pragma endregion
 
@@ -61,7 +61,7 @@ __singleton __nothrow void __stackcall data_collision_thread() noexcept {
 
     size_t iterations = 0x1;
 
-    while (thread_state_flag) 
+    while (thread_state_flag)
     {
         for (auto i = 0; i < data_size; ++i)
             reinterpret_cast<byte*>(data_ptr)[i] = static_cast<std::uint8_t>(NULL); // reset data
@@ -120,7 +120,7 @@ __singleton __nothrow std::int32_t __stackcall main() noexcept {
         const auto& hash32 = qhash::qhash32(&us, sizeof(us));
 
         const auto& hash64 = qhash::qhash64(&us, sizeof(us));
-        
+
         hash_collection_32.push_back(hash32.hash);
 
         hash_collection_64.push_back(hash64.hash);
@@ -136,11 +136,11 @@ __singleton __nothrow std::int32_t __stackcall main() noexcept {
 
 #pragma endregion
 
-    std::size_t collision_counter32 = static_cast<std::size_t>( NULL );
+    std::size_t collision_counter32 = static_cast<std::size_t>(NULL);
 
-    std::size_t collision_counter64 = static_cast<std::size_t>( NULL );
+    std::size_t collision_counter64 = static_cast<std::size_t>(NULL);
 
-    std::size_t collision_counterCRC = static_cast<std::size_t>( NULL );
+    std::size_t collision_counterCRC = static_cast<std::size_t>(NULL);
 
     std::vector<collision_t> collisions_detected_vector32{};
 
@@ -153,25 +153,25 @@ __singleton __nothrow std::int32_t __stackcall main() noexcept {
     const auto collision_inlined_test = [
 
         &hash_collection_32,
-        &hash_collection_64,
-        &hash_collection_CRC,
-        &collision_counter32,
-        &collision_counter64,
-        &collision_counterCRC,
-        &collisions_detected_vector32,
-        &collisions_detected_vector64,
-        &collisions_detected_vectorCRC
-    ](  
+            &hash_collection_64,
+            &hash_collection_CRC,
+            &collision_counter32,
+            &collision_counter64,
+            &collision_counterCRC,
+            &collisions_detected_vector32,
+            &collisions_detected_vector64,
+            &collisions_detected_vectorCRC
+    ](
 
         const std::int32_t collision_list_index,
         std::int32_t& ii,
         std::int32_t& xx
-    ) noexcept {   //  0 = 32, 1 = 64, 2 = crc32
+        ) noexcept {   //  0 = 32, 1 = 64, 2 = crc32
 
-        switch (collision_list_index) {
+            switch (collision_list_index) {
 
             case 0: { // qhash32 list call
-                
+
                 if (hash_collection_32[ii] == hash_collection_32[xx]) {
 
                     bool is_collision_contained = false;
@@ -267,26 +267,26 @@ __singleton __nothrow std::int32_t __stackcall main() noexcept {
                 break;
             }
 
-        }
+            }
 
         };
 
 #pragma region Algorithm output collision testing
 
-    for (std::int32_t i = 0; i <= USHORT_MAX; ++i) {
+        for (std::int32_t i = 0; i <= USHORT_MAX; ++i) {
 
-        for (std::int32_t x = 0; x <= USHORT_MAX; ++x) {
+            for (std::int32_t x = 0; x <= USHORT_MAX; ++x) {
 
-            if (x == i) {   //  skip checking the current entry in the array against itself as this is illogical
+                if (x == i) {   //  skip checking the current entry in the array against itself as this is illogical
 
-                continue;
+                    continue;
+                }
+
+                for (auto w = 0; w < 3; ++w)
+                    collision_inlined_test(w, i, x);
+
             }
-
-            for(auto w = 0 ; w < 3; ++w)
-                collision_inlined_test(w, i, x);
-
         }
-    }
 
 #pragma region Macros
 
@@ -300,62 +300,62 @@ __singleton __nothrow std::int32_t __stackcall main() noexcept {
 
 #pragma endregion
 
-    STDOUT_PRINTBLOCK_SEPERATOR();
+        STDOUT_PRINTBLOCK_SEPERATOR();
 
-    std::cout << "[+] Done, qhash32 collision counter: " << collision_counter32 << std::endl;
+        std::cout << "[+] Done, qhash32 collision counter: " << collision_counter32 << std::endl;
 
-    std::int32_t iterator = static_cast<std::uint32_t>(NULL);
+        std::int32_t iterator = static_cast<std::uint32_t>(NULL);
 
-    std::ios_base::fmtflags std_flag;
+        std::ios_base::fmtflags std_flag;
 
-    for (const auto& cdtc32 : collisions_detected_vector32) {
+        for (const auto& cdtc32 : collisions_detected_vector32) {
 
-        SAVE_STDOUT_FLAGS(std_flag);
+            SAVE_STDOUT_FLAGS(std_flag);
 
-        std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtc32.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtc32.index_x  << " and, 0x" << cdtc32.index_y << std::endl;
+            std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtc32.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtc32.index_x << " and, 0x" << cdtc32.index_y << std::endl;
 
-        SET_STDOUT_INTEGRAL(std_flag);
+            SET_STDOUT_INTEGRAL(std_flag);
 
-        ++iterator;
-    }
+            ++iterator;
+        }
 
-    STDOUT_PRINTBLOCK_SEPERATOR();
+        STDOUT_PRINTBLOCK_SEPERATOR();
 
-    iterator = 0;   //  reset iterator
+        iterator = 0;   //  reset iterator
 
-    std::cout << "[+] Done, qhash64 collision counter: " << collision_counter64 << std::endl;
+        std::cout << "[+] Done, qhash64 collision counter: " << collision_counter64 << std::endl;
 
-    for ( const auto& cdtc64 : collisions_detected_vector64 ) {
+        for (const auto& cdtc64 : collisions_detected_vector64) {
 
-        SAVE_STDOUT_FLAGS(std_flag);
+            SAVE_STDOUT_FLAGS(std_flag);
 
-        std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtc64.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtc64.index_x << " and, 0x" << cdtc64.index_y << std::endl;
+            std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtc64.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtc64.index_x << " and, 0x" << cdtc64.index_y << std::endl;
 
-        SET_STDOUT_INTEGRAL(std_flag);
+            SET_STDOUT_INTEGRAL(std_flag);
 
-        ++iterator;
-    }
+            ++iterator;
+        }
 
-    STDOUT_PRINTBLOCK_SEPERATOR();
+        STDOUT_PRINTBLOCK_SEPERATOR();
 
-    iterator = 0;   //  reset iterator
+        iterator = 0;   //  reset iterator
 
-    std::cout << "[+] Done, CRC32 collision counter: " << collision_counterCRC << std::endl;
+        std::cout << "[+] Done, CRC32 collision counter: " << collision_counterCRC << std::endl;
 
-    for (const auto& cdtcCRC : collisions_detected_vectorCRC) {
+        for (const auto& cdtcCRC : collisions_detected_vectorCRC) {
 
-        SAVE_STDOUT_FLAGS(std_flag);
+            SAVE_STDOUT_FLAGS(std_flag);
 
-        std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtcCRC.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtcCRC.index_x << " and, 0x" << cdtcCRC.index_y << std::endl;
+            std::cout << "[+] Detection# " << iterator << ": Hash value of 0x" << PRINT_HEXADECIMAL << hash_collection_32[cdtcCRC.index_x] << " was detected as a match between differential 16-bit origin data entries of value(s): 0x" << cdtcCRC.index_x << " and, 0x" << cdtcCRC.index_y << std::endl;
 
-        SET_STDOUT_INTEGRAL(std_flag);
+            SET_STDOUT_INTEGRAL(std_flag);
 
-        ++iterator;
-    }
+            ++iterator;
+        }
 
 #pragma endregion
 
-    std::cin.get();
+        std::cin.get();
 
-    return 0;
+        return 0;
 }
